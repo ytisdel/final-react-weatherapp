fb70e20ec88a0a4357f8f33e29830df7cb264675
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-02-25 10:47:21 -0700

    cleaned and removed unnecessary things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Weather App</h1>
+      <footer>
+        The app was made by Lona Tisdel and is <a href="/">open-sourced</a>
+      </footer>
     </div>
   );
 }
